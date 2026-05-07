276084ab8e64b70d80f1a1dc07285c880af42416
tikitko <bns.6587@gmail.com>
2026-05-07 12:58:46 +0200

    fix compile

--- src/handle_image.rs
+++ src/handle_image.rs
@@ -17,18 +17,15 @@ pub async fn handle_image(
 
     let result = if !Path::new(&image_type.local_path(&base64_url)).exists() {
         tokio::task::spawn_blocking(move || {
-            let lock = |storage: &Mutex<HashMap<String, flume::Receiver<ProcessResult>>>| {
-                storage.lock().unwrap_or_else(|e| e.into_inner())
-            };
             let res_rx = {
-                let maybe_rx = lock(&in_progress_storage).get(&base64_url).cloned();
+                let maybe_rx = in_progress_storage.lock().unwrap_or_else(|e| e.into_inner()).get(&base64_url).cloned();
                 if let Some(res_rx) = maybe_rx {
                     res_rx
                 } else {
                     let (res_tx, res_rx) = flume::bounded(1);
-                    lock(&in_progress_storage).insert(base64_url.clone(), res_rx.clone());
+                    in_progress_storage.lock().unwrap_or_else(|e| e.into_inner()).insert(base64_url.clone(), res_rx.clone());
                     if tx.send((image_type, base64_url.clone(), res_tx)).is_err() {
-                        lock(&in_progress_storage).remove(&base64_url);
+                        in_progress_storage.lock().unwrap_or_else(|e| e.into_inner()).remove(&base64_url);
                         return Err(Box::from("worker thread is dead"));
                     }
                     res_rx
@@ -44,7 +41,7 @@ pub async fn handle_image(
                     }
                 }
             };
-            lock(&in_progress_storage).remove(&base64_url);
+            in_progress_storage.lock().unwrap_or_else(|e| e.into_inner()).remove(&base64_url);
             result
         })
         .await
